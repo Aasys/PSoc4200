0d91bf31b635c9f65c17504680bb2d94b91facb3
Aasys Sresta <aasysresta@gmail.com>
2018-02-15 07:04:42 -0500

    L05P01 Complete

--- L05P01-MeasuringElapsedTime.cydsn/main.c
+++ L05P01-MeasuringElapsedTime.cydsn/main.c
@@ -1,12 +1,6 @@
 /* ========================================
- *
- * Copyright YOUR COMPANY, THE YEAR
- * All Rights Reserved
- * UNPUBLISHED, LICENSED SOFTWARE.
- *
- * CONFIDENTIAL AND PROPRIETARY INFORMATION
- * WHICH IS THE PROPERTY OF your company.
- *
+ * Aasys Sresta
+ * 02/15/2018
  * ========================================
 */
 #include "project.h"
@@ -24,26 +18,55 @@ uint32 t0; // timer count
 CY_ISR(InterruptHandler) {
     // Clear Interrupt, CC: Compare/Capture
     Timer_ClearInterrupt(Timer_INTR_MASK_CC_MATCH);
+    
+    // First interrupt | init
     if (InterruptCnt==0)
         t0=Timer_ReadCapture();
     else {
+        // string print int to string of ascii
+        // diff of current and previous
         sprintf(buffer, "%d", (int)(Timer_ReadCapture()-t0));
         UART_UartPutString(buffer);
-        UART_UartPutChar(0x20);
+        UART_UartPutChar(0x20); // ascii space
         t0 = Timer_ReadCapture();
     }
-    InterruptCnt++;
+    InterruptCnt++; // increment # of interrupts
 }
 
 int main(void)
 {
     CyGlobalIntEnable; /* Enable global interrupts. */
-
-    /* Place your initialization/startup code here (e.g. MyInst_Start()) */
-
+    
+    // set interrupt address
+    CC_ISR_StartEx(InterruptHandler);
+    UART_Start();
+    
+    // After reset the board put string
+    UART_UartPutString(10); // ascii new line
+    UART_UartPutString(13); // ascii return
+    UART_UartPutString("Press button to start");
+    UART_UartPutString(10); // ascii new line
+    UART_UartPutString(13); // ascii return
+    
+    // stuck on switch press and release    
+    while (Capture_Pin_Read());
+    while (!Capture_Pin_Read());
+    
+    PWM_Start(); // start PWM
+    
+    Timer_Start();
+    
+    // wait for 10 captures, i.e. 9 diffs
+    while (InterruptCnt < NUM_Capture);
+    
+    // Stop when done
+    Timer_Stop();
+    CC_ISR_Disavle();
+    PWM_Stop();
+    
+    // control loop
     for(;;)
-    {
-        /* Place your application code here. */
+    { 
     }
 }
 
